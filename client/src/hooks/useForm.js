f11237ea8d5bda2fda4c95254c4a0e38d9a880f1
jonathan warner <j.m.warners@gmail.com>
2020-08-07 09:46:24 -0700

    debugged and got plants list to show up

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,13 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+ export function useForm(initialForm) {
+    const [ formState, setFormState ] = useState(initialForm);
+    const handleChange = e => {
+        setFormState({
+            ...formState, 
+            [e.target.name]: e.target.value,
+        });
+    };
+    return[formState, handleChange]
+}
